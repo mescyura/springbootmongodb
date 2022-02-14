e0b0f83257563e69627add9f1e743debdc79d94f
YuriiHolyda <mescyura@gmail.com>
2022-02-14 20:05:49 +0200

    added endpoint for get-by-id and update-by-id api add logging

--- src/main/java/com/example/springbootmongodb/controller/TodoController.java
+++ src/main/java/com/example/springbootmongodb/controller/TodoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -20,10 +21,11 @@ public class TodoController {
 
     @GetMapping("/todos")
     public ResponseEntity<?> getAllTodos() {
+        log.info("getting all todos");
         List<TodoDTO> todos = todoRepository.findAll();
-        log.info("all todos - {}",todos);
         if (todos.size() > 0) {
-            return new ResponseEntity<List<TodoDTO>>(todos, HttpStatus.OK);
+            log.info("all todos - {}", todos);
+            return new ResponseEntity<>(todos, HttpStatus.OK);
         } else {
             return new ResponseEntity<>("no todos available", HttpStatus.NOT_FOUND);
         }
@@ -31,13 +33,44 @@ public class TodoController {
 
     @PostMapping(value = "/todos", consumes = "application/json")
     public ResponseEntity<?> createTodo(@RequestBody TodoDTO todoDTO) {
-        log.info("created todo - {}",todoDTO);
+        log.info("created todo - {}", todoDTO);
         try {
             todoDTO.setCreatedAt(new Date(System.currentTimeMillis()));
             todoRepository.save(todoDTO);
-            return new ResponseEntity<TodoDTO>(todoDTO, HttpStatus.OK);
+            return new ResponseEntity<>(todoDTO, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getTodoById(@PathVariable("id") String id) {
+        log.info("Searching for todo with id - {}", id);
+        Optional<TodoDTO> todoDTOOptional = todoRepository.findById(id);
+        if (todoDTOOptional.isPresent()) {
+            log.info("todo found - {}", todoDTOOptional.get());
+            return new ResponseEntity<>(todoDTOOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id" + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<?> updateTodoById(@PathVariable("id") String id, @RequestBody TodoDTO todoDTO) {
+        log.info("Searching for todo with id - {}", id);
+        Optional<TodoDTO> todoDTOOptional = todoRepository.findById(id);
+        if (todoDTOOptional.isPresent()) {
+            log.info("todo found - {}", todoDTOOptional.get());
+            TodoDTO todoDTOtoSave = todoDTOOptional.get();
+            todoDTOtoSave.setCompleted(todoDTO.getCompleted() != null ? todoDTO.getCompleted() : todoDTOtoSave.getCompleted());
+            todoDTOtoSave.setTodo(todoDTO.getTodo() != null ? todoDTO.getTodo() : todoDTOtoSave.getTodo());
+            todoDTOtoSave.setDescription(todoDTO.getDescription() != null ? todoDTO.getDescription() : todoDTOtoSave.getDescription());
+            todoDTOtoSave.setUpdatedAt(new Date(System.currentTimeMillis()));
+            todoRepository.save(todoDTOtoSave);
+            log.info("todo updated - {}", todoDTOtoSave);
+            return new ResponseEntity<>(todoDTOtoSave, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id" + id, HttpStatus.NOT_FOUND);
         }
     }
 }
